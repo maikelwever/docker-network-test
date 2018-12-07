7c0f3262d81a5462e868cc315e979ecf010de2ed
Maikel Wever <maikelwever@gmail.com>
2018-12-07 17:20:46 +0100

    Better error handling, lower timeout

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,24 @@ extern crate trust_dns_resolver;
 use trust_dns_resolver::Resolver;
 use trust_dns_resolver::config::*;
 
+use core::time::Duration;
+use std::io::{Result, Error, ErrorKind};
 
-fn do_lookup() -> Result<(), std::io::Error> {
-    let resolver = Resolver::new(ResolverConfig::cloudflare(), ResolverOpts::default())?;
-    let response = resolver.lookup_ip("cloudflare.com.")?;
 
-    let _ = response.iter().next().expect("no addresses returned");
+fn do_lookup() -> Result<()> {
+    let mut opts = ResolverOpts::default();
 
-    Ok(())
+    // The actual runtime timeout is not even close to 2 seconds,
+    // it takes about 12 seconds in a container with broken iptables rules.
+    opts.timeout = Duration::from_secs(2);
+    opts.attempts = 1;
+    let resolver = Resolver::new(ResolverConfig::cloudflare(), opts)?;
+    let response = resolver.ipv4_lookup("cloudflare.com.")?;
+
+    match response.iter().next() {
+        Some(_) => Ok(()),
+        None => Err(Error::new(ErrorKind::Other, "No results returned")),
+    }
 }
 
 fn main() {
